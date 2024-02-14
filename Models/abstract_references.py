30f972e4e6a4e89c2cc72d4dc59b0c452dbdec97
jezv <jezv@yandex.ru>
2024-02-14 20:51:02 +0800

    fix

--- Models/abstract_references.py
+++ Models/abstract_references.py
@@ -14,6 +14,7 @@ class abstract_referance(ABC):
         self.name = name
         self.__id = uuid.uuid4()
 
+
     @property
     def name(self):
         return self.__name.strip()
--- main.py
+++
@@ -1,6 +0,0 @@
-from Src.settings_manager import settings_manager
-
-manager = settings_manager()
-manager.open('settings.json')
-print(manager)
-print(manager.settings)
