655b28992ff40b95c62a6a1077b390c8ebcffa85
jezv <jezv@yandex.ru>
2024-02-29 16:13:29 +0800

    report_csv

--- Src/Logics/report_csv.py
+++ Src/Logics/report_csv.py
@@ -1,16 +1,17 @@
 from Src.Logics.report import report
-from Utils import typecheck, attribute
+from Utils import typecheck
 
 
 class report_csv(report):
 
     @typecheck
     def create(self, storage_key: str):
+        result = []
+        headers = self._storage.data[storage_key][0].get_attr_keys('head')
+        result.append(';'.join(headers))
+        for model in self._storage.data[storage_key]:
+            result.append(';'.join(map(str, model.get_attr_values('head'))))
+        return '\n'.join(result)
 
-        for obj in self._storage.data[storage_key]:
-            for x in dir(type(obj)):
-                if hasattr(getattr(type(obj), x), 'head'):
-                    print('\n', x)
-                    print('value:',getattr(obj, x))
-                    print('head:',getattr(getattr(type(obj), x), 'head'))
+        
                 
--- Tests/test_reports.py
+++ Tests/test_reports.py
@@ -20,9 +20,5 @@ class test_models(unittest.TestCase):
 
         csv = report_csv(factory.storage, manager.settings)
 
-        result1 = csv.create(storage.unit_key())
-        result3 = csv.create(storage.nomenculature_key())
-        # result2 = csv.create(storage.group_key())
-        # result4 = csv.create(storage.recipe_key())
-        # print(result1)
-        assert True == True
+        assert csv.create(storage.unit_key()) != ''
+        assert csv.create(storage.nomenculature_key()) != ''
